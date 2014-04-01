b01d3de4384c4b9ca746ca03ecbc93cb701ed1dd
Santiago Munín <burning1@gmail.com>
2014-04-01 16:17:29 +0200

    Starting testing environment

---
+++ app/src/test/java/se/chalmers/agile/LoginTest.java
@@ -0,0 +1,14 @@
+package se.chalmers.agile;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class LoginTest {
+    @Test
+    public void testTrueIsTrue() throws Exception {
+        assertEquals(true, true);
+    }
+}
