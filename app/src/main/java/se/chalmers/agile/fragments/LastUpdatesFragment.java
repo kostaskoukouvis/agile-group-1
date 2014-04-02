5b56ffe9df1855c7dbca37d4f7e499a3f7d7e399
Pavel Rabetski <rabeckijps@gmail.com>
2014-04-02 18:25:37 +0200

    add github plumbing

--- app/src/main/java/se/chalmers/agile/fragments/LastUpdatesFragment.java
+++ app/src/main/java/se/chalmers/agile/fragments/LastUpdatesFragment.java
@@ -10,17 +10,27 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import org.eclipse.egit.github.core.Commit;
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.client.PageIterator;
+import org.eclipse.egit.github.core.service.CommitService;
+
+import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 import se.chalmers.agile.R;
-import se.chalmers.agile.core.Commit;
 
 /**
  * Displays the last updates from the selected repositories.
  */
 public class LastUpdatesFragment extends ListFragment {
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +39,14 @@ public class LastUpdatesFragment extends ListFragment {
         //1. Get the list of project/branches
         //List<ProjectBranch> branches = ...;
         //2. Fetch data from GitHub
+
         List<Commit> commits = new LinkedList<Commit>();
-        commits.add(new Commit("agile", "master", new Date(), "john doe", "Hello world"));
+
+        CommitService cs = new CommitService();
+        IRepositoryIdProvider repositoryId = RepositoryId.create("marcyb5st", "agile-group-1");
+        PageIterator<RepositoryCommit> commitPages = cs.pageCommits(repositoryId, "testingEnvironment", null, 10);
+        Collection<RepositoryCommit> lastTenCommits = commitPages.next();
+
         //3. Filtering
 
         //4. Display results
@@ -63,21 +79,19 @@ public class LastUpdatesFragment extends ListFragment {
                 LayoutInflater inflater = ((Activity) context).getLayoutInflater();
                 row = inflater.inflate(layoutResourceId, parent, false);
             }
-            Commit c = data[position];
+            Commit commit = data[position];
 
             TextView branch = (TextView) row.findViewById(R.id.commit_branch);
-            branch.setText(c.getBranch());
+            branch.setText("~");
 
             TextView date = (TextView) row.findViewById(R.id.commit_date);
-            if (c.getTime()!=null) {
-                date.setText(c.getTime().toGMTString());
-            }
+            date.setText(dateFormat.format(commit.getCommitter().getDate()));
 
             TextView message = (TextView) row.findViewById(R.id.commit_message);
-            message.setText(c.getCommitMsg());
+            message.setText(commit.getMessage());
 
             TextView author = (TextView) row.findViewById(R.id.commit_user);
-            author.setText(c.getCommitter());
+            author.setText(commit.getCommitter().getName());
 
             return row;
         }
