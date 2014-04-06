6dfc3be0725ad2f5665909fdc064ef377a985a8d
Daniel Jonsson <damajo90@gmail.com>
2014-04-06 21:39:37 +0200

    Added comments to the new timer

--- app/src/main/java/timer/CountDownTimer.java
+++ app/src/main/java/timer/CountDownTimer.java
@@ -2,37 +2,52 @@ package timer;
 
 import android.os.Handler;
 import android.view.MenuItem;
-
 import se.chalmers.agile.R;
 
 /**
- * Created by Daniel on 2014-04-06.
+ * A countdown timer that counts down from a time given when created.
  */
 public class CountDownTimer extends Thread {
 
+    //Singleton instance
     private static CountDownTimer instance = null;
+
+    //Representation of the time state of the timer
     private long millis;
     private long millisUntilFinished;
     private long interval;
+
+    //Menu items which are to be manipulated
     private MenuItem subPause;
     private MenuItem subStart;
     private MenuItem subReset;
     private static MenuItem timer;
+
+    //States of the timer
     private boolean running;
     private boolean finished;
+
+    //Handler to pass information from the timer thread to the UI thread
     private Handler handler;
 
 
+    /**
+     *Private constructor to create a countdown timer.
+     */
     private CountDownTimer(long millisInFuture, long countDownInterval, Handler handler) {
         super();
-        this.millis = millisInFuture;
-        this.interval = countDownInterval;
-        this.millisUntilFinished = millisInFuture;
-        this.running = false;
-        this.finished = false;
+        millis = millisInFuture;
+        interval = countDownInterval;
+        millisUntilFinished = millisInFuture;
+        running = false;
+        finished = false;
         this.handler = handler;
 
     }
+
+    /**
+     *Method to get singleton object. Create a new object if instance is null, else return instance.
+     */
 
     public static CountDownTimer getInstance(long millisInFuture, long countDownInterval, Handler handler){
         if(instance == null){
@@ -41,6 +56,10 @@ public class CountDownTimer extends Thread {
         return instance;
     }
 
+
+    /**
+     *Overridden run method from Thread. Defines what should happen when the timer run.
+     */
     @Override
     public void run(){
         while(true){
@@ -64,53 +83,72 @@ public class CountDownTimer extends Thread {
         }
     }
 
+    /**
+     *Start the timer for the first time. If the thread is already alive, only update the timer menu item.
+     */
     public void startTimer(MenuItem timer){
         this.timer = timer;
-        this.subPause = timer.getSubMenu().findItem(R.id.timerPause);
-        this.subStart = timer.getSubMenu().findItem(R.id.timerStart);
-        this.subReset = timer.getSubMenu().findItem(R.id.timerReset);
+        subPause = timer.getSubMenu().findItem(R.id.timerPause);
+        subStart = timer.getSubMenu().findItem(R.id.timerStart);
+        subReset = timer.getSubMenu().findItem(R.id.timerReset);
 
         if(!this.isAlive()) {
             running = true;
-            this.start();
+            start();
         }
 
         if(running){
-            this.subStart.setVisible(false);
-            this.subPause.setVisible(true);
+            subStart.setVisible(false);
+            subPause.setVisible(true);
         }
         else{
-            this.subStart.setVisible(true);
-            this.subPause.setVisible(false);
+            subStart.setVisible(true);
+            subPause.setVisible(false);
         }
     }
 
+    /**
+     *Pauses the timer
+     */
     public void pauseTimer(){
         subPause.setVisible(false);
         subStart.setVisible(true);
         running = false;
     }
 
+    /**
+     *Resumes the timer
+     */
     public void resumeTimer(){
         subStart.setVisible(false);
         subPause.setVisible(true);
         running = true;
     }
 
+
+    /**
+     *Resets the timer
+     */
     public void resetTimer(){
         subPause.setVisible(true);
         subStart.setVisible(false);
-        this.millisUntilFinished = millis;
-        this.finished = false;
+        millisUntilFinished = millis;
+        finished = false;
         running = true;
     }
 
+
+    /**
+     *Check if the timer is running
+     */
     public boolean isRunning(){
         return running;
     }
 
+    /**
+     *Updates the timer text. Sends information to the handler to forward information to the UI thread.
+     */
     public void onTick(final long millisUntilFinished) {
-
         final Runnable myRunnable = new Runnable() {
             public void run() {
                 timer.setTitle("Navigator swap in: " + (millisUntilFinished / 1000) / 60 +
@@ -121,6 +159,9 @@ public class CountDownTimer extends Thread {
     }
 
 
+    /**
+     * Runs when the time is up
+     */
     public void onFinish(){
         subPause.setVisible(false);
         running = false;
