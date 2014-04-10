0d29d9a7419dafe392665843767bf0ab575bbdd1
Marcello Steiner <steiner.marcello@gmail.com>
2014-04-10 13:25:26 +0300

    sfuyweyugewryugsgyusdf<

--- app/src/main/java/se/chalmers/agile/fragments/RepositoryFragment.java
+++ app/src/main/java/se/chalmers/agile/fragments/RepositoryFragment.java
@@ -223,10 +223,12 @@ public class RepositoryFragment extends ListFragment implements Serializable {
         }
 
 
-
-
-
         
+
+
+
+
+
         @Override
         public Repository getItem(int position) {
             return repos.get(position);
