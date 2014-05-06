bfed1d4acafa3201a243bafc11179ab06ff55ec3
Konstantinos Koukouvis <koskouk87@gmail.com>
2014-05-06 11:26:44 +0200

    Update fix test

--- app/src/main/java/se/chalmers/agile/fragments/SettingsFragment.java
+++ app/src/main/java/se/chalmers/agile/fragments/SettingsFragment.java
@@ -1,5 +1,6 @@
 package se.chalmers.agile.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
@@ -8,8 +9,9 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import se.chalmers.agile.R;
+import se.chalmers.agile.activities.MainActivity;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     public static SettingsFragment createInstance() {
         SettingsFragment fragment = new SettingsFragment();
@@ -25,5 +27,24 @@ public class SettingsFragment extends PreferenceFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         LinearLayout l = (LinearLayout) inflater.inflate(R.layout.settings, container, false);
         return l;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals("auto-update")){
+            ((MainActivity) getActivity()).registerReceiver();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
